2ad201aa40c0539b1f0bc8237613a914825a0f90
Victor <a.victor.alecu@gmail.com>
2025-03-13 17:06:30 +0100

    Also added some files that were excluded for some reason.

---
+++ trainOperator/src/main/java/org/group21/trainoperator/TrainOperatorApplication.java
@@ -0,0 +1,13 @@
+package org.group21.trainoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainOperatorApplication.class, args);
+	}
+
+}
